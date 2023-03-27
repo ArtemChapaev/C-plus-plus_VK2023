e0d0dce32a0193e740c8f95b36cea1bb29a8c7d4
Artem <ayuchapaev@gmail.com>
2023-03-27 15:54:54 +0300

    added desructor

--- include/operations.hpp
+++ include/operations.hpp
@@ -8,6 +8,8 @@ namespace Operations {
 
 class IOperation {
    public:
+    virtual ~IOperation() {}
+
     virtual void AddInputData(const std::string &str) = 0;
     virtual void ProcessLine(const std::string &str) = 0;
     virtual void SetNextOperation(const std::shared_ptr<IOperation> &operation) = 0;
